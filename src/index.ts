74162e21d6e471454b1f882b2957134121448801
techniponi <wincam97@gmail.com>
2020-01-31 01:11:55 -0600

    Initialize webpack and typescript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-// test
 console.log("test");
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src", "index.ts"),
+  watch: true,
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, "src", "assets"),
+        to: "assets"
+      }
+    ]),
+    new HTMLWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html"
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+    chunkFilename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join("./dist/"),
+    historyApiFallback: true,
+    inline: true,
+    host: "0.0.0.0",
+    port: 8080,
+    compress: true,
+    disableHostCheck: true
+  }
+};
