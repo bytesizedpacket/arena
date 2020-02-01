97997e502b656d762c75221fc1722d77392ff1cc
techniponi <wincam97@gmail.com>
2020-02-01 01:36:49 -0600

    Player has score attribute

--- src/player.ts
+++ src/player.ts
@@ -4,13 +4,10 @@ import { Entity } from "./entity";
 
 // useful variables
 let statusDiv = document.getElementById("status");
-export enum State {
-  ACTIVE,
-  DEAD
-}
 
 // main player object
 export class Player extends Entity {
+  public score: number;
   constructor(
     spriteObject: Sprite,
     app: Application,
@@ -18,12 +15,21 @@ export class Player extends Entity {
     displayHealthBar?: boolean
   ) {
     super(spriteObject, app, speed, displayHealthBar);
+    this.score = 0;
   }
 
+  // this will run every frame
   public tick() {
     super.tick();
     if (this.health <= 0) {
       statusDiv.innerHTML = "Uh-oh spaghetti-o's! You're <b>dead.</b>";
     }
+  }
+
+  // sets the player's current score
+  public setScore(score: number) {
+    this.score = score;
+    // TODO: render this within the game window
+    statusDiv.innerHTML = "Current Score: " + this.score;
   }
 }
