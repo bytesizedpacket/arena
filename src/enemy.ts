167df03ab23e4304f635c9c48be3dd65b51d4f72
techniponi <wincam97@gmail.com>
2020-02-02 03:03:11 -0600

    murderfuck dead enemies

--- src/enemy.ts
+++ src/enemy.ts
@@ -2,6 +2,8 @@ import { Sprite } from "pixi.js";
 import { Application } from "pixi.js";
 import { Entity } from "./entity";
 import { player } from "./index";
+import { app } from "./index";
+import { entities } from "./index";
 
 // main enemy object
 export class Enemy extends Entity {
@@ -18,7 +20,20 @@ export class Enemy extends Entity {
   public tick() {
     super.tick();
     if (this.health <= 0) {
-      player.setScore(player.score++);
+      player.setScore(player.score + 1);
+
+      // we need to destroy this now :c
+      app.stage.removeChild(this.spriteObject);
+      app.stage.removeChild(this.healthBar);
+      entities.splice(entities.indexOf(this), 1);
+      this.healthBar.destroy();
+      this.spriteObject.destroy();
     }
+  }
+
+  // we have been clicked!
+  public onClick(e?: Event) {
+    super.onClick(e);
+    this.health -= 25;
   }
 }
